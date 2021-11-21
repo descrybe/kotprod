32f3142cd64b6cbb552a7c77b20eccaf06ed77d0
descrybe <alexander.inkeev@deleteagency.com>
2021-11-21 03:41:16 +0300

    added new expanded-video component, fixed some minor warnings and styles

--- src/components/video-grid/js/video-item.jsx
+++ src/components/video-grid/js/video-item.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import VideoExpanded from '../../video-expanded';
+import VideoPreview from './video-preview';
 
-class VideoItem extends React.PureComponent {
+class VideoItem extends React.Component {
     constructor() {
         super();
-
+        
         this.state = {
             expanded: false,
         }
     }
 
-    _expandVideoOnClick() {
+    _expandVideoOnClick = () => {
         this.setState({
             expanded: true,
         });
     }
 
     render() {
+        console.log(this.state)
         return (
-            <li className='video-category__list-item'>
-                <div onClick={this._expandVideoOnClick}>{this.props.video}</div>
-                <VideoExpanded />
+            <li className='video-category__list-item'
+                onClick={this._expandVideoOnClick}>
+                <VideoPreview 
+                    video={this.props.video}/>
+                <VideoExpanded opened={this.state.expanded}/>
             </li>
         )
     }
